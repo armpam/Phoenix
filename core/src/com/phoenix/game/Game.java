d76a5e644fa336f56bb2e89efd37346e296dfcb5
alesd <alesanderppal@gmail.com>
2017-02-21 14:29:21 +0100

    Creada la clase de la pantalla principal (GameScreen) Cambiados los extends e implements para que se ajusten a un juego.

--- core/src/com/phoenix/game/Game.java
+++ core/src/com/phoenix/game/Game.java
@@ -1,33 +1,27 @@
 package com.phoenix.game;
 
-import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.phoenix.game.Screens.GameScreen;
 
-public class Game extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+public class Game extends com.badlogic.gdx.Game {
+	public SpriteBatch batch;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+        setScreen(new GameScreen(this));
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		super.render(); //Delega el renderizado a la pantalla que esté activa
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
 	}
 }
