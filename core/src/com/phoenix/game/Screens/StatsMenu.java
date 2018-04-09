1f317adc5c9f39c60bc0e24b140ec665a4fce641
alesd <alesanderppal@gmail.com>
2018-04-10 00:38:15 +0200

    Progreso

--- core/src/com/phoenix/game/Screens/StatsMenu.java
+++ core/src/com/phoenix/game/Screens/StatsMenu.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
 import com.phoenix.game.Entities.MainCharacter;
 import com.phoenix.game.Game;
 
